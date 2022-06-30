7dcfb538825ca404069d7fe31f5a572f9021ae3f
ZHOU <zhouyu990507@163.com>
2022-06-30 10:59:24 +0800

    update 2022-06-30

--- TypeScript/code/01_hello_ts/03.ts
+++ TypeScript/code/01_hello_ts/03.ts
@@ -17,3 +17,21 @@ let p1: custom_obj = {
 let p2: custom_obj2 = p1
 
 console.log(p2);
+
+// 1. 直接声明函数
+function id<T>(value: T): T {
+    return value
+}
+
+// 2. 先定义函数的类型，再声明函数 
+type CustomFunc = <T>(value: T, value2?: T) => T
+let func: CustomFunc = (value) => {
+    return value
+}
+let num: string = func<string>("123")
+console.log(num);
+
+let num1 = id(10);
+let str1 = id('123');
+
+console.log(num1)
