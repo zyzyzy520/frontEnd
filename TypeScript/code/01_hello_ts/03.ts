8be91032a701566e2d363c23862c33417931e4fc
ZHOU <zhouyu990507@163.com>
2022-07-01 09:23:20 +0800

    update 2022-07-01

--- TypeScript/code/01_hello_ts/03.ts
+++ TypeScript/code/01_hello_ts/03.ts
@@ -35,3 +35,31 @@ let num1 = id(10);
 let str1 = id('123');
 
 console.log(num1)
+
+function id2<T>(value: T[]): T[] {
+    console.log(value.length)   //此处会报错
+    return value
+}
+
+id2([1, 2, 3, 4]);
+
+interface ILength {
+    length: number
+}
+
+let arr3: ILength = [1, 2, 3];
+console.log(arr3, arr3.length);
+
+interface IdFunc<T> {
+    id: (value: T) => T,
+    ids: () => T[]
+}
+
+let obj: IdFunc<number> = {
+    id: (value) => {
+        return value
+    },
+    ids: () => {
+        return [1, 2, 3, 4]
+    }
+}
